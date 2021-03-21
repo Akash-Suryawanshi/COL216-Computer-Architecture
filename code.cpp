cf3dd822f3786cca4a95068f5e547f06b2d8b1b2
Akash Suryawanshi <suryaakash7799@gmail.com>
2021-03-21 13:52:00 +0530

    show-cycles

--- code.cpp
+++ code.cpp
@@ -314,6 +314,7 @@ int main(int argc, char const* argv[])
 			exit(0);
 		}
 		counter++;
+		cout << cycles_clock << ", " << endl;
 	}
 	dram_memory[row_number] = row_buffer;
 	cout << "Number of Clock Cycles: " << cycles_clock << endl;
